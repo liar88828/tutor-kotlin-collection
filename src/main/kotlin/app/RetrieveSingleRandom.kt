1f0b861632b198661169c6b8ba6c0dfeee25efc7
brian <liar88828@gmail.com>
2023-06-27 22:49:33 +0700

    27 Retrieve Single Random

---
+++ src/main/kotlin/app/RetrieveSingleRandom.kt
@@ -0,0 +1,10 @@
+package app
+
+fun main() {
+	val range = (0..10).toList()
+	println(range.random())
+	println(range.random())
+	println(range.random())
+	println(range.random())
+	println(range.random())
+}
