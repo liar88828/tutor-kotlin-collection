2a00a7e7e6060fe32843deb6d90b8a20ea35ab15
brian <liar88828@gmail.com>
2023-06-28 12:50:37 +0700

    41 Destructuring Declaration map

---
+++ src/main/kotlin/app/DestructuringMap.kt
@@ -0,0 +1,12 @@
+package app
+
+fun main() {
+
+	val map = mapOf(
+		"a" to "Eko", "b" to "Kurniawan", "c" to "Khannedy"
+	)
+	for ((key, value) in map) println("$key : $value")
+	println()
+//	map.forEach { entry -> println("${entry.component1()} :${entry.component2()}") }
+	map.forEach { (key, value) -> println("$key : $value") }
+}
