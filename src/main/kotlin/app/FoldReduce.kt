a714b3aa2c43ef1e18b43d1aa832ac3ebe7b5dd6
brian <liar88828@gmail.com>
2023-06-28 11:19:31 +0700

    34 Fold and reduce

---
+++ src/main/kotlin/app/FoldReduce.kt
@@ -0,0 +1,31 @@
+package app
+
+fun main() {
+	val numbers = (0..100).toList().shuffled()
+
+	val max = numbers.reduce { first, second ->
+		if (first > second) first
+		else second
+	}
+	println(max)
+
+	val min = numbers.reduce { first, second ->
+		if (first < second) first
+		else second
+	}
+	println(min)
+
+	val sum = numbers.fold(0) { first, second ->
+		first + second
+	}
+	println(sum)
+	println(numbers.sum())
+
+
+	val names = listOf<String>("Eko", "Kurniawan", "Khannedy")
+	val count = names.fold(0) { first, second ->
+		first + 1
+	}
+	println(count)
+	println(names.count())
+}
